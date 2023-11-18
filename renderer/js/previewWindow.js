2d38a8a3507de59314952b8f6ff730d598baaf12
dandr94 <d.andreev1994@gmail.com>
2023-11-18 19:51:17 +0200

    Add various functionalities to the previewWindow

--- renderer/js/previewWindow.js
+++ renderer/js/previewWindow.js
@@ -1,23 +1,99 @@
-window.api.on("file-info", (fileInfo) => {
-    console.log(`file-info in renderer.js:`, fileInfo);
+const addRecipientIcon = document.getElementById("add-recipient");
 
-    // Set subject and message values
+addRecipientIcon.addEventListener("click", () => addRecipientInput());
+
+function addRecipientInput() {
+    const recipientsList = document.getElementById("recipients");
+
+    const newRecipientInput = document.createElement("input");
+    newRecipientInput.type = "text";
+    newRecipientInput.placeholder = "Enter recipient";
+    newRecipientInput.addEventListener("keydown", (event) =>
+        handleRecipientKeydown(event)
+    );
+
+    const cancelIcon = createCancelIcon("", null, null);
+    cancelIcon.addEventListener("click", () =>
+        newRecipientInput.parentElement.remove()
+    );
+
+    const listItem = document.createElement("li");
+    listItem.appendChild(newRecipientInput);
+    listItem.appendChild(cancelIcon);
+
+    recipientsList.appendChild(listItem);
+
+    newRecipientInput.focus();
+}
+
+function handleRecipientKeydown(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+
+        const enteredRecipient = event.target.value.trim();
+        if (enteredRecipient) {
+            const recipientsList = document.getElementById("recipients");
+
+            const cancelIcon = createCancelIcon(enteredRecipient, null, null);
+            cancelIcon.addEventListener("click", () => listItem.remove());
+
+            const listItem = document.createElement("li");
+            listItem.textContent = enteredRecipient;
+            listItem.appendChild(cancelIcon);
+
+            recipientsList.appendChild(listItem);
+
+            event.target.parentElement.remove();
+        } else {
+            event.target.parentElement.remove();
+        }
+    }
+}
+
+function createCancelIcon(recipient, listItem, fileInfo) {
+    const cancelIcon = document.createElement("i");
+    cancelIcon.classList.add("cancel-icon", "fa-solid", "fa-xmark");
+    cancelIcon.addEventListener("click", () =>
+        removeRecipient(recipient, listItem, fileInfo)
+    );
+    return cancelIcon;
+}
+
+function removeRecipient(recipient, listItem, fileInfo) {
+    listItem.remove();
+
+    const index = fileInfo.recipients.indexOf(recipient);
+    if (index !== -1) {
+        fileInfo.recipients.splice(index, 1);
+    }
+}
+
+function createListItem(recipient, fileInfo) {
+    const listItem = document.createElement("li");
+    const recipientContainer = document.createElement("span");
+    recipientContainer.textContent = recipient;
+    recipientContainer.appendChild(
+        createCancelIcon(recipient, listItem, fileInfo)
+    );
+    listItem.appendChild(recipientContainer);
+    listItem.dataset.recipient = recipient;
+    return listItem;
+}
+
+function addRecipientToList(recipient, fileInfo) {
+    const recipientsList = document.getElementById("recipients");
+    const listItem = createListItem(recipient, fileInfo);
+    recipientsList.appendChild(listItem);
+}
+
+window.api.on("file-info", (fileInfo) => {
     document.getElementById("subject").value = fileInfo.subject;
     document.getElementById("message").value = fileInfo.message;
 
-    // Clear existing recipients
     const recipientsList = document.getElementById("recipients");
     recipientsList.innerHTML = "";
 
-    // Add new recipients
     fileInfo.recipients.forEach((recipient) => {
-        addRecipientToList(recipient);
+        addRecipientToList(recipient, fileInfo);
     });
-
-    // Function to add a recipient to the list
-    function addRecipientToList(recipient) {
-        const listItem = document.createElement("li");
-        listItem.textContent = recipient;
-        recipientsList.appendChild(listItem);
-    }
 });
